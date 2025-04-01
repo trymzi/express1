ae354c5aa31568c2cf4e3695df8ab74dff6141e3
Ryma <oumansourryma@gmail.com>
2025-04-01 12:12:47 -0400

    routes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<hrml><body><h1>This is an express server</h1></body></hrml>');
